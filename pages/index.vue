f5c95ad9a9c075b3f0a49c40a30cc6038fdf2738
abe <abecai@gmail.com>
2025-04-09 02:28:08 +0800

    refactor(index.vue): improve code formatting and update repository path

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script setup>
-const { data: experiments } = await useAsyncData('/', 
+const { data: experiments } = await useAsyncData('/',
   () => queryContent('experiments')
-    .where({ $or: [ 
-      { status: { $exists: false } }, 
-      { status: 'published' },
-    ] })
+    .where({
+      $or: [
+        { status: { $exists: false } },
+        { status: 'published' },
+      ]
+    })
     .sort({ date: -1 })
     .find(),
 )
@@ -29,7 +31,7 @@ function getSlugFromExperiment(experiment) {
 }
 
 function getTitleFromExperiment(experiment) {
-  return experiment.title 
+  return experiment.title
     ?? getSlugFromExperiment(experiment).split('-')
       .map(capitalize)
       .join(' ')
@@ -38,10 +40,10 @@ function getTitleFromExperiment(experiment) {
 function capitalize(word) {
   if (word.length === 0) {
     return word
-  } 
+  }
   else if (word.length === 1) {
     return word.toUpperCase()
-  } 
+  }
   else {
     return word[0].toUpperCase() + word.slice(1)
   }
@@ -52,29 +54,21 @@ function getThumbnailFromExperiment(experiment) {
 }
 
 function getRepoPathFromExperiment(experiment) {
-  return `https://github.com/Tresjs/lab/tree/main/components/content/${getSlugFromExperiment(experiment)}`
+  return `https://github.com/vootaa/3DVL/tree/main/components/content/${getSlugFromExperiment(experiment)}`
 }
 
 function getRepoTitleFromExperiment(experiment) {
-  return `${getSlugFromExperiment(experiment)} – code on Github` 
+  return `${getSlugFromExperiment(experiment)} – code on Github`
 }
 </script>
 
 <template>
   <main class="bg-white dark:bg-dark">
     <div class="w-full container mx-auto px-4 md:px-0 pb-60 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-16 pt-8">
-      <TheCard
-        v-for="experiment in formattedExperiments"
-        :key="experiment._path"
-        :title="experiment.title"
-        :path="experiment._path"
-        :repo-title="experiment.repoTitle"
-        :repo-path="experiment.repoPath"
-        :media="experiment.thumbnail"
-        :description="experiment.description"
-        :author="experiment.author"
-        :tags="experiment.tags"
-      />
+      <TheCard v-for="experiment in formattedExperiments" :key="experiment._path" :title="experiment.title"
+        :path="experiment._path" :repo-title="experiment.repoTitle" :repo-path="experiment.repoPath"
+        :media="experiment.thumbnail" :description="experiment.description" :author="experiment.author"
+        :tags="experiment.tags" />
     </div>
   </main>
 </template>
