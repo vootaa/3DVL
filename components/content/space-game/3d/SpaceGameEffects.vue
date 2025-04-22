dab9eb62640c98a60f751cee1479db14f7454ed4
abe <abecai@gmail.com>
2025-04-22 12:47:03 +0800

    fix: update import paths for postprocessing effects and improve responsive sizing logic

--- components/content/space-game/3d/SpaceGameEffects.vue
+++ components/content/space-game/3d/SpaceGameEffects.vue
@@ -1,16 +1,23 @@
 <script lang="ts" setup>
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
-import { OutputPass } from 'three/examples/jsm/postprocessing/OutputPass'
-import { UnrealBloomPass } from 'three-stdlib'
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js'
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js'
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js'
 import { extend, useLoop, useTres } from '@tresjs/core'
-import { shallowRef } from 'vue'
+import { shallowRef, watch } from 'vue'
+import { Vector2 } from 'three'
 
 extend({ EffectComposer, OutputPass, UnrealBloomPass, RenderPass })
 const { renderer, scene, camera, sizes } = useTres()
 const composer = shallowRef<EffectComposer>()
 
-useLoop().render(({ elapsed }) => {
+watch([() => sizes.width.value, () => sizes.height.value], () => {
+  if (composer.value) {
+    composer.value.setSize(sizes.width.value, sizes.height.value)
+  }
+})
+
+useLoop().render(({ }) => {
   if (composer.value) {
     composer.value!.render()
   }
@@ -20,7 +27,7 @@ useLoop().render(({ elapsed }) => {
 <template>
   <TresEffectComposer ref="composer" :args="[renderer]" :set-size="[sizes.width.value, sizes.height.value]">
     <TresRenderPass :args="[scene, camera]" attach="passes-0" />
-    <TresUnrealBloomPass :args="[undefined, 0.5, 0.1, 0]" attach="passes-1" />
+    <TresUnrealBloomPass :args="[new Vector2(sizes.width.value, sizes.height.value), 0.5, 0.1, 0]" attach="passes-1" />
     <TresOutputPass attach="passes-2" :set-size="[sizes.width.value, sizes.height.value]" />
   </TresEffectComposer>
 </template>
