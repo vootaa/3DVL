9cb6b6ccb4039bfe8d7a362873f8ffbf75b76f21
abe <abecai@gmail.com>
2025-06-13 05:10:39 +0800

    refactor: remove ambient and directional light components in Lights.vue; streamline lighting setup

--- components/content/shadertoy-museum/components/Lights.vue
+++ components/content/shadertoy-museum/components/Lights.vue
@@ -52,16 +52,4 @@ useLoop().onBeforeRender(
 
 <template>
   <TresGroup ref="lightsGroup" />
-
-  <!-- Enhanced but not overly bright ambient lighting -->
-  <TresAmbientLight :intensity="0.4" color="#ffffff" />
-
-  <!-- Main directional light source - moderate intensity -->
-  <TresDirectionalLight :position="[10, 10, 5]" :intensity="1.2" color="#ffffff" :cast-shadow="true" />
-
-  <!-- Auxiliary light source -->
-  <TresDirectionalLight :position="[-5, 8, -3]" :intensity="0.6" color="#f0f8ff" />
-
-  <!-- Top soft light -->
-  <TresPointLight :position="[0, 15, 0]" :intensity="0.8" :distance="50" color="#ffffff" />
 </template>
