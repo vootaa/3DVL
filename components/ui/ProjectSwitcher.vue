b0c0a03df4d34cfab26e8f89c18d3dd5c2248272
abe <abecai@gmail.com>
2025-07-01 00:08:21 +0800

    refactor: unify project listing logic in ProjectSwitcher component

--- components/ui/ProjectSwitcher.vue
+++ components/ui/ProjectSwitcher.vue
@@ -10,22 +10,24 @@ const currentProject = computed(() => {
   return projects.find(p => p.path === route.path) || null;
 });
 
-const otherProjects = computed(() => {
-  return projects.filter(p => p.path !== route.path);
+const projectsToList = computed(() => {
+  // If on a project page, show other projects. Otherwise (on homepage), show all.
+  return currentProject.value
+    ? projects.filter(p => p.path !== route.path)
+    : projects;
 });
 </script>
 
 <template>
   <div class="relative">
     <button
-      v-if="currentProject"
       @click="showPanel = !showPanel"
       class="flex items-center gap-2 px-3 py-2 text-sm font-medium text-white bg-gray-800/50 backdrop-blur-sm rounded-md hover:bg-gray-700/70 transition-colors"
     >
       <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24">
         <path fill="currentColor" d="M16 20h4v-4h-4m-6 0h4v-4h-4m-6 0h4v-4H4m12-6h4V4h-4m-6 0h4V4h-4m-6 0h4V4H4v4Z" />
       </svg>
-      <span>{{ currentProject.name }}</span>
+      <span>{{ currentProject ? currentProject.name : 'Switch Project' }}</span>
     </button>
     <Transition
       enter-active-class="transition duration-100 ease-out"
@@ -37,7 +39,7 @@ const otherProjects = computed(() => {
     >
       <div v-if="showPanel" class="absolute bottom-full right-0 mb-2 w-64 origin-bottom-right bg-gray-900/80 backdrop-blur-md border border-gray-700 rounded-lg shadow-lg">
         <ul class="p-2">
-          <li v-for="project in otherProjects" :key="project.id">
+          <li v-for="project in projectsToList" :key="project.id">
             <NuxtLink :to="project.path" @click="showPanel = false" class="block px-3 py-2 text-sm text-gray-300 hover:bg-gray-700/50 hover:text-white rounded-md">
               {{ project.name }}
             </NuxtLink>
