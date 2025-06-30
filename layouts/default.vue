41f3fe1f18a3cf2ea6ce906e83e6fcbd8f7b4fd2
abe <abecai@gmail.com>
2025-07-01 01:10:26 +0800

    fix: wrap ThemeSwitcher in ClientOnly for improved rendering

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,9 @@ import ProjectSwitcher from '~/components/ui/ProjectSwitcher.vue';
 <template>
   <div>
     <header class="fixed top-5 right-5 z-50 flex items-center gap-4">
-      <ThemeSwitcher />
+      <ClientOnly>
+        <ThemeSwitcher />
+      </ClientOnly>
       <SocialLinks />
     </header>
     <main>
