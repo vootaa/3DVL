a732f7e4dfeb3887cbf6ec4703c70ab760e26dc4
abe <abecai@gmail.com>
2025-07-12 03:32:30 +0800

    feat: integrate Petersen Graph connections into Tethers component for dynamic arch visualization

--- components/projects/petersen-expanse/components/scene/Tethers.vue
+++ components/projects/petersen-expanse/components/scene/Tethers.vue
@@ -3,7 +3,7 @@ import { ref, computed, onMounted, watch } from 'vue'
 import { Vector3, BufferAttribute } from 'three'
 import { useRenderLoop } from '@tresjs/core'
 
-import { tetherConfig } from '../../configs/tether-config'
+import { tetherConfig, tetherConnections } from '../../configs/tether-config'
 import { getCurrentLODLevel } from '../../configs/lodlevel-config'
 
 import tetherVertexShader from '../../shaders/tether-vertex.glsl'
@@ -28,31 +28,9 @@ const props = withDefaults(defineProps<Props>(), {
 })
 
 const cameraDistance = ref(1000)
-
 const tetherGeometryRef = ref()
 const tetherMaterialRef = ref()
 const tetherPointsRef = ref()
-
-// Petersen Graph connections (60 edges total: 30 forward + 30 reverse)
-const petersenConnections = computed(() => {
-  const connections = []
-  
-  // Forward connections (arching upward)
-  for (let i = 0; i < 10; i++) {
-    connections.push({ from: i, to: (i + 5) % 10, direction: 'forward', archDirection: 1 })
-    connections.push({ from: i, to: i + 10, direction: 'forward', archDirection: 1 })
-    connections.push({ from: i + 10, to: ((i + 1) % 10) + 10, direction: 'forward', archDirection: 1 })
-  }
-  
-  // Reverse connections (arching downward)
-  for (let i = 0; i < 10; i++) {
-    connections.push({ from: (i + 5) % 10, to: i, direction: 'reverse', archDirection: -1 })
-    connections.push({ from: i + 10, to: i, direction: 'reverse', archDirection: -1 })
-    connections.push({ from: ((i + 1) % 10) + 10, to: i + 10, direction: 'reverse', archDirection: -1 })
-  }
-  
-  return connections
-})
 
 const tetherAttributes = computed(() => {
   const positions: number[] = []
@@ -61,32 +39,35 @@ const tetherAttributes = computed(() => {
   const tetherIds: number[] = []
   const archParams: number[] = []
   const chaoticPositions: number[] = []
-  
+
   if (props.stellarCorePositions.length < 20) {
     return null
   }
-  
-  petersenConnections.value.forEach((connection, tetherIndex) => {
-    const fromPos = props.stellarCorePositions[connection.from] || new Vector3()
-    const toPos = props.stellarCorePositions[connection.to] || new Vector3()
-    
+
+  let tetherIndex = 0
+
+  // process forward connections
+  tetherConnections.forward.forEach((connection) => {
+    const fromPos = props.stellarCorePositions[connection[0]] || new Vector3()
+    const toPos = props.stellarCorePositions[connection[1]] || new Vector3()
+
     // Create arch particles between nodes
     const particleCount = tetherConfig.particlesPerTether
     for (let i = 0; i < particleCount; i++) {
       const t = i / (particleCount - 1)
-      
-      // Quadratic Bézier curve for arch
+
+      // Quadratic Bézier curve for arch (upward)
       const midPoint = fromPos.clone().lerp(toPos, 0.5)
-      midPoint.y += connection.archDirection * tetherConfig.archHeight
-      
+      midPoint.y += tetherConfig.archHeight // Adjust height for upward arch
+
       const pos = new Vector3()
         .copy(fromPos)
         .multiplyScalar((1 - t) * (1 - t))
         .add(midPoint.clone().multiplyScalar(2 * (1 - t) * t))
         .add(toPos.clone().multiplyScalar(t * t))
-      
+
       positions.push(pos.x, pos.y, pos.z)
-      
+
       // Generate chaotic initial position
       const chaoticRadius = Math.random() * 1000
       const chaoticAngle = Math.random() * Math.PI * 2
@@ -96,24 +77,69 @@ const tetherAttributes = computed(() => {
         chaoticHeight,
         Math.sin(chaoticAngle) * chaoticRadius
       )
-      
-      // Color based on direction
-      const color = connection.direction === 'forward' 
-        ? tetherConfig.colors.forward
-        : tetherConfig.colors.reverse
+
+      // Forward color (cyan)
+      const color = tetherConfig.colors.forward
       colors.push(color.r, color.g, color.b)
-      
+
       // Alpha varies along the arch
       const alpha = Math.sin(t * Math.PI) * tetherConfig.baseOpacity
       alphas.push(alpha)
-      
+
       tetherIds.push(tetherIndex)
-      archParams.push(connection.archDirection, t)
+      archParams.push(1, t) // archDirection: 1 (upward), progress: t
     }
+
+    tetherIndex++
   })
 
-  // Import BufferAttribute from three
-  // (add this import at the top if not present: import { BufferAttribute } from 'three')
+  // process reverse connections
+  tetherConnections.reverse.forEach((connection) => {
+    const fromPos = props.stellarCorePositions[connection[0]] || new Vector3()
+    const toPos = props.stellarCorePositions[connection[1]] || new Vector3()
+
+    // Create arch particles between nodes
+    const particleCount = tetherConfig.particlesPerTether
+    for (let i = 0; i < particleCount; i++) {
+      const t = i / (particleCount - 1)
+
+      // Quadratic Bézier curve for arch (downward)
+      const midPoint = fromPos.clone().lerp(toPos, 0.5)
+      midPoint.y -= tetherConfig.archHeight // Adjust height for downward arch
+
+      const pos = new Vector3()
+        .copy(fromPos)
+        .multiplyScalar((1 - t) * (1 - t))
+        .add(midPoint.clone().multiplyScalar(2 * (1 - t) * t))
+        .add(toPos.clone().multiplyScalar(t * t))
+
+      positions.push(pos.x, pos.y, pos.z)
+
+      // Generate chaotic initial position
+      const chaoticRadius = Math.random() * 1000
+      const chaoticAngle = Math.random() * Math.PI * 2
+      const chaoticHeight = (Math.random() - 0.5) * 500
+      chaoticPositions.push(
+        Math.cos(chaoticAngle) * chaoticRadius,
+        chaoticHeight,
+        Math.sin(chaoticAngle) * chaoticRadius
+      )
+
+      // Reverse color (orange)
+      const color = tetherConfig.colors.reverse
+      colors.push(color.r, color.g, color.b)
+
+      // Alpha varies along the arch
+      const alpha = Math.sin(t * Math.PI) * tetherConfig.baseOpacity
+      alphas.push(alpha)
+
+      tetherIds.push(tetherIndex)
+      archParams.push(-1, t) // archDirection: -1 (downward), progress: t
+    }
+
+    tetherIndex++
+  })
+
   return {
     position: new BufferAttribute(new Float32Array(positions), 3),
     color: new BufferAttribute(new Float32Array(colors), 3),
@@ -174,14 +200,8 @@ onMounted(() => {
 <template>
   <TresGroup v-if="enabled && tetherAttributes">
     <TresPoints ref="tetherPointsRef">
-      <TresBufferGeometry 
-        ref="tetherGeometryRef"
-        :attributes="tetherAttributes"
-      />
-      <TresShaderMaterial 
-        ref="tetherMaterialRef"
-        v-bind="tetherShader"
-      />
+      <TresBufferGeometry ref="tetherGeometryRef" :attributes="tetherAttributes" />
+      <TresShaderMaterial ref="tetherMaterialRef" v-bind="tetherShader" />
     </TresPoints>
   </TresGroup>
 </template>
